9c48c74dde8484ad6526d19d3cda5479f35bffe7
Bhavya Tripathi <bhavyatripathi00@gmail.com>
2021-11-12 13:29:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Assignment 1 was successful!');
+  res.send('THIS IS A NEW COMMIT!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
