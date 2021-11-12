ffadb9e89cb494980336edf0b6f9a4ac233b2ab4
Bhavya Tripathi <bhavyatripathi00@gmail.com>
2021-11-12 14:19:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World! A new commit was made! It should trigger a Jenkins build.');
+  res.send('Hello World! A new commit was made! It should trigger a Jenkins Build!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
