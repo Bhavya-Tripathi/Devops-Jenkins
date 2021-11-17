243d9e01ac5b0ab386d3ff76db71b7ab827f3dd5
Bhavya Tripathi <bhavyatripathi00@gmail.com>
2021-11-17 19:58:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Assignment 1 was successful!');
+  res.send('Assignment 1 was successful!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
